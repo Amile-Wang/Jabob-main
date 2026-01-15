4f6a4dff352bc91f1f050cddbe7be1a1dca78271
Amile_Wang <1420947175@qq.com>
2026-01-15 09:44:00 +0800

    modified:   main/application.cc

--- main/application.cc
+++ main/application.cc
@@ -855,11 +855,11 @@ void Application::SetDeviceState(DeviceState state) {
                     // std::ref(Lang::Sounds::P3_0),
                     std::ref(Lang::Sounds::P3_1),
                     std::ref(Lang::Sounds::P3_2),
-                    std::ref(Lang::Sounds::P3_3),
-                    std::ref(Lang::Sounds::P3_4),
-                    std::ref(Lang::Sounds::P3_5),
-                    std::ref(Lang::Sounds::P3_6),
-                    std::ref(Lang::Sounds::P3_7)
+                    // std::ref(Lang::Sounds::P3_3),
+                    // std::ref(Lang::Sounds::P3_4),
+                    // std::ref(Lang::Sounds::P3_5),
+                    // std::ref(Lang::Sounds::P3_6),
+                    // std::ref(Lang::Sounds::P3_7)
                 };
                 
                 // 生成随机索引并播放随机音效
